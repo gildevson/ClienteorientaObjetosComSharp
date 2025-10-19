c3511d79e5c21657802b3852c61d0f7e57cb5deb
gildevson <gildevson@gmail.com>
2025-10-19 18:56:18 -0300

    first commit

---
+++ ClientesOBJETOS/AdicionarProduto.cs
@@ -0,0 +1,57 @@
+﻿using ClasseProduto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ClasseAdicionar
+{
+    class AdicionandoProduto
+    {
+        public int Adicionar(Produto Adicionaproduto)
+        {
+            Console.Write("Digite a quantidade dos produtos a acrescentar: ");
+            int acrescentando = int.Parse(Console.ReadLine());
+
+            int quantidadeExistente = Adicionaproduto.quantidade;
+            int soma = quantidadeExistente + acrescentando;
+
+            Adicionaproduto.quantidade = soma; // atualiza o valor dentro do objeto
+
+            if (soma > quantidadeExistente)
+            {
+                Console.WriteLine("Produto adicionado com sucesso!");
+            }
+
+            return soma; // retorna a nova quantidade total
+
+
+        }
+
+
+        public int Remover(Produto produto)
+        {
+            Console.WriteLine("Digite a quantidade dos produtos a remover: ");
+            int quantidadeRemovida = int.Parse(Console.ReadLine());
+
+            int quantidadeExistente = produto.quantidade;
+
+            int subtracao = quantidadeExistente - quantidadeRemovida;
+
+            produto.quantidade = subtracao; // atualiza o valor dentro do objeto
+
+
+            if (quantidadeExistente < subtracao)
+            {
+                Console.WriteLine("Produto removido com sucesso!");
+            }
+
+            return subtracao; // retorna a nova quantidade total
+        }
+
+    }
+
+}
---
+++ ClientesOBJETOS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using ClasseProduto;
+using ClasseAdicionar;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.Write("Nome do produto: ");
+            produto.nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produto.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade inicial: ");
+            produto.quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\ Produto cadastrado: {produto.nome}, {produto.quantidade} unidades, Preço: R${produto.preco:F2}\n");
+
+            AdicionandoProduto manipulador = new AdicionandoProduto();
+
+            manipulador.Adicionar(produto);
+            Console.WriteLine($"Quantidade atual: {produto.quantidade}");
+
+            manipulador.Remover(produto);
+            Console.WriteLine($"Quantidade atual: {produto.quantidade}");
+        }
+    }
+}
